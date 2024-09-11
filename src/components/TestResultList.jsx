d10071121e993f75a4212dde198264cbe1b6d2c0
WonbinLee <wonbin145@gmail.com>
2024-09-11 15:17:34 +0900

    [style]검색결과 로딩, 오류가운데 정렬

--- src/components/TestResultList.jsx
+++ src/components/TestResultList.jsx
@@ -8,11 +8,11 @@ const TestResultList = () => {
   const { data: results, isPending, isError } = useGetTestResultsQuery();
 
   if (isPending) {
-    return <div className="flex items-center">로딩 중입니다...</div>;
+    return <div className="flex justify-center items-center">로딩 중입니다...</div>;
   }
 
   if (isError) {
-    return <div className="flex items-center">오류가 발생했습니다.</div>;
+    return <div className="flex justify-center items-center">오류가 발생했습니다.</div>;
   }
 
   // results의 객체 중 visibility가 true인 객체 또는 userId가 로그인한 userId와 일치하는 결과들만 나열하기
